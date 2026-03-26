1042bc735b7e9dba092adc8f2538e2198c2df461
Fabio <ganumfilhofabio@gmail.com>
2026-03-26 08:36:39 -0300

    definicao das structs principais

--- main.c
+++ main.c
@@ -2,16 +2,39 @@
 #include <stdlib.h>
 
 typedef struct cabecalho_s{
-char tipo;
-int topo, proxRRN, nroEstacoes, nroParesEstacoes;
+    char tipo;
+    int topo;
+    int proxRRN;
+    int nroEstacoes;
+    int nroParesEstacoes;
 };
 
-
+typedef struct registro_s {
+    char removido;
+    int proximo;
+    int CodEstacao;
+    int CodLinha;
+    int CodProxEst;
+    int distProxEstacao;
+    int codLinhaIntegra;
+    int codEstIntegra;
+    int tamNomeEstacao;
+    char *NomeEstacao;
+    int tamNomeLinha;
+    char *NomeLinha;
+};
 
 
 
 
 int main () {
 
+    char *nomeArquivo = calloc(10, sizeof(char));
+    FILE *file = fopen(nomeArquivo, "r");
+    free(nomeArquivo);
+
+
+
+    fclose(file);
     return 0;
 }
